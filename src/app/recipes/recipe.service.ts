8cf98038c47fed99d385a60f96925e7059fde68d
jtodd3 <jtodd@vendasta.com>
2020-03-08 17:14:56 -0600

    use Subjects instead of EventEmitters

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,10 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
-
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is a test', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.826.620.suffix/1537973085542.jpeg', [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]),
     new Recipe('Another Test Recipe', 'This is a test', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.826.620.suffix/1537973085542.jpeg', [new Ingredient('Buns', 2), new Ingredient('Burger', 4)]),
